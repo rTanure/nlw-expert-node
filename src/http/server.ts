943614cc3f1d75fe8189741a2878de9f55c0f4b9
Richardy Tanure <richardy.tanure@hotmail.com>
2024-02-12 01:44:45 -0300

    verificação com zod, salvando na base de dados e fim da primeira aula

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,30 @@
+import { PrismaClient } from "@prisma/client"
 import fastify from "fastify"
+import z from "zod"
+
 const app = fastify()
 
-app.get("/hello", ()=>{
-  return '{nome: "Richardy"}'
+// Conectando com o prisma
+const prisma = new PrismaClient()
+
+app.post('/polls', async (request, reply)=>{
+  // Modelo de autenticação do zod
+  const createPollBody = z.object({
+    title: z.string()
+  })
+
+  // Capturando valores autenticados pelo zod
+  const { title } = createPollBody.parse(request.body)
+
+  // Salvando no banco de dados e recuperando retorno
+  const poll = await prisma.poll.create({
+    data: {
+      title,
+    }
+  })
+
+  // Retornando para o usuário o que foi calculado
+  return reply.status(201).send({ pollId: poll.id })
 })
 
 app.listen({ port: 3333 }).then(()=> {
