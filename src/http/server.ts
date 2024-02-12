fc422f5734ee9eed35a14a0f7495a74c8379f9df
Richardy Tanure <richardy.tanure@hotmail.com>
2024-02-12 01:18:50 -0300

    docker configurado

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,10 @@
 import fastify from "fastify"
 const app = fastify()
 
+app.get("/hello", ()=>{
+  return '{nome: "Richardy"}'
+})
+
 app.listen({ port: 3333 }).then(()=> {
   console.log("HTTP server running!")
 })
